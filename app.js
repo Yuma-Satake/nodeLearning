035afac26899c61808c75e43391d4ba797b705d2
Yuma Satake <satous.y@icloud.com>
2023-06-29 23:20:39 +0900

    1

--- app.js
+++ app.js
@@ -1,46 +1,45 @@
-const express = require('express');
+const express = require("express");
 const nodemailer = require("nodemailer");
-const xoauth2 = require('xoauth2');
+const xoauth2 = require("xoauth2");
 
 const app = express();
 
-app.get('/',(req,res)=>{
-    function sendMails() {
+app.get("/", (req, res) => {
+  function sendMails() {
+    const gmail = {
+      user: "",
+      clientId: "",
+      clientSecret: "",
+      refreshToken: "",
+    };
 
-        // const gmail = {
-        //     user:'',
-        //     clientId:"",
-        //     clientSecret:"",
-        //     refreshToken:""
-        // }
+    const generator = function () {
+      var g = xoauth2.createXOAuth2Generator(gmail);
+      g.on("token", function (token) {
+        console.log(`new token  ${token.user} ${token.accessToken}`);
+      });
+      return g;
+    };
 
-        // const generator = function() {
-        //   var g = xoauth2.createXOAuth2Generator(gmail);
-        //   g.on('token',function(token){
-        //     console.log(`new token  ${token.user} ${token.accessToken}`);
-        //   })
-        //   return g;
-        // }
+    const transporter = nodemailer.createTransport({
+      service: "Gmail",
+      // auth: {xoauth2: generator()}
+      auth: {
+        user: "satake.satake.y@gmail.com",
+        pass: "",
+      },
+    });
 
-        const transporter = nodemailer.createTransport({
-          service: 'Gmail',
-          // auth: {xoauth2: generator()}
-          auth: {
-            user: 'satake.satake.y@gmail.com',
-            pass: ''
-          }
-        });
+    transporter.sendMail({
+      from: "satake.satake.y@gmail.com",
+      to: "satake.satake.y@gmail.com",
+      subject: "testmail",
+      text: "this is a test",
+    });
+  }
 
-        transporter.sendMail({
-          from: "satake.satake.y@gmail.com",
-          to: "satake.satake.y@gmail.com",
-          subject: "testmail",
-          text: "this is a test"
-        });
-    }
-
-    sendMails();
-    res.render('hello.ejs');
+  sendMails();
+  res.render("hello.ejs");
 });
 
 app.listen(3000);
